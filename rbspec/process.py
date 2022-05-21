321da8ece13fe0f8c49bf335748c7ba2f1e1fad1
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2022-05-21 13:48:45 -0300

    Update process.py

--- rbspec/process.py
+++ rbspec/process.py
@@ -1,11 +1,4 @@
 from . import tools
 
-log = None
-
-def main(self):
-
-  self.log = load_log()
-  tools.load_env()
-  
-  if __name__ == '__main__':
-    main()
+log = tools.load_log()
+tools.load_env()

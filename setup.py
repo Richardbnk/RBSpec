4d33b386fdfae63e82a476446ac625d0101b4800
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2022-05-21 15:52:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='rbspec',
-    version='1.0.28',
+    version='1.0.29',
     author="Richard Raphael Banak",
     description="Robot Specialist Library for Robot Process Automation",
     url="https://github.com/Richardbnk/RBSpec",

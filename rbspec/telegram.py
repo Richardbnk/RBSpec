cf8651f7562410bd58ed8c31c668da43468b9213
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2022-05-27 21:19:33 -0300

    Create telegram.py

---
+++ rbspec/telegram.py
@@ -0,0 +1,25 @@
+"""
+# Developer: Richard Raphael Banak
+# Objective: Functions to help send messages through Telegram 
+# Creation date: 2022-05-26
+#
+# NEW BOT: Include bot on any group, the look for any updates on:
+#    https://api.telegram.org/bot<YourBOTToken>/getUpdates
+"""
+
+import telegram
+import os
+import urllib
+import json
+
+
+
+def send(
+    message,
+    chat_id,
+    bot_token,
+):
+    global BOT_API_TOKEN
+
+    bot = telegram.Bot(token=bot_token)
+    bot.sendMessage(chat_id=chat_id, text=message)

25a79775087d921d9bf8d6f2b6e771a016cbdc8e
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2022-07-16 13:10:05 -0300

    Update navigation.py

--- rbspec/rpa/navigation.py
+++ rbspec/rpa/navigation.py
@@ -114,7 +114,7 @@ def get_image_position(image_path, timeout=None):
 
 def click_at_image(image_path, timeout=None):
     x, y = get_image_position(image_path=image_path, timeout=timeout)
-    nv.click(x=x, y=y)
+    click(x=x, y=y)
     
     
 def wait_for_image(image_path, timeout=None):
